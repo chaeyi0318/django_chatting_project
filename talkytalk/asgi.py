eff285b510f0d2d73037b28c214851c9b50f65d9
Chaeyi <chaeyi0318@gmail.com>
2024-10-04 00:00:06 +0900

    feat: 채팅 기능 관련

--- talkytalk/asgi.py
+++ talkytalk/asgi.py
@@ -14,3 +14,20 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talkytalk.settings')
 
 application = get_asgi_application()
+
+# channels 라우팅과 미들웨어는 django 초기화 이후에 가져오기
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+import chatting.routing
+
+application = ProtocolTypeRouter({
+    'http': application,
+    'websocket': AuthMiddlewareStack(
+            AllowedHostsOriginValidator(
+            URLRouter(
+                chatting.routing.websocket_urlpatterns
+            )     
+        ),
+    ),
+})
